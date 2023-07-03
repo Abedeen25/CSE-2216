bcff2e82a21576dd974f1d756e280c1692eb8048
MD Ashiqur Rahman Joy <44981422+Abedeen25@users.noreply.github.com>
2023-07-04 01:27:41 +0600

    Linked Lists' operations

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -12,22 +12,76 @@ public:
     Node* next;
 };
 
+Node* LL_insert_to_beginning(Node* head,int newData){ //needs assignment
+    Node* newNode = new Node();
+    newNode -> data = newData;
+    newNode -> next = head;
+    return newNode;
+}
+
+void LL_insert_to_beginning(Node** head,int newData){ //One without assignment needed
+    Node* newNode = new Node();
+    newNode -> data = newData;
+    newNode -> next = *head;
+    *head = newNode;
+}
+
+void LL_insert_after(Node* head, int newData, int target){
+    Node* temp = head;
+    while(temp -> next != NULL && temp -> data != target){
+        temp = temp -> next;
+    }
+    Node* newNode = new Node();
+    newNode -> data = newData;
+    newNode -> next = temp -> next;
+    temp -> next = newNode;
+}
+
+void LL_insert_at_end(Node* head, int newData){
+    Node* temp = head;
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    }
+    Node* newNode = new Node();
+    newNode -> data = newData;
+    temp -> next = newNode;
+}
+
+void LL_delete_node(Node** head, int hasValue){
+    Node* temp = *head;
+    if(temp -> data == hasValue){
+        *head = temp -> next;
+        free(temp);
+        return;
+    }
+    Node* prevNode;
+    while(temp != NULL && temp -> data != hasValue){
+        prevNode = temp;
+        temp = temp -> next;
+    }
+    if(temp == NULL){
+        cout << "The item doesn't exist \n";
+        return;
+    }
+    prevNode -> next = temp -> next;
+    free(temp);
+}
+
 int main(){
     Node* head;
-    Node* E1 = NULL;
-    Node* E2 = NULL;
 
     head = new Node();
-    E1 = new Node();
-    E2 = new Node();
-
     head -> data = 1;
-    E1 -> data = 2;
-    E2 -> data = 3;
 
-    head ->next = E1;
-    E1 -> next = E2;
 
+    head = LL_insert_to_beginning(head,3);
+    LL_insert_to_beginning(&head, 4);
+
+    LL_insert_after(head,2,3);
+
+    LL_insert_at_end(head, 0);
+
+    LL_delete_node(&head,10);
     Node* Cursor;
     Cursor = head;
     while(Cursor){
